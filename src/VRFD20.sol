c692d6c74d1bb91364e75fda0e757cbf0d2d3e6f
allen <allenmuhani@gmail.com>
2026-01-15 16:16:51 +0300

    added .vscode fiolder

---
+++ src/VRFD20.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+
+contract VRFD20 is VRFConsumerBaseV2Plus {}
