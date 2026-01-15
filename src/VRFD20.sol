0581883183a74d61ee0cec79b9dd3c5230feaf9c
allen <allenmuhani@gmail.com>
2026-01-15 17:04:40 +0300

    initialized CRF consumer base plus

--- src/VRFD20.sol
+++ src/VRFD20.sol
@@ -2,6 +2,15 @@
 
 pragma solidity ^0.8.0;
 
-import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import { VRFConsumerBaseV2Plus } from
+    "chainlink-brownie-contracts/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 
-contract VRFD20 is VRFConsumerBaseV2Plus {}
+contract VRFD20 is VRFConsumerBaseV2Plus {
+    uint256 public s_subscriptionId;
+
+    constructor(uint256 subscriptionId, address vrfCoordinator)
+        VRFConsumerBaseV2Plus(vrfCoordinator)
+    {
+        s_subscriptionId = subscriptionId;
+    }
+}
