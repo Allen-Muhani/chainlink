2e55349bd33956c0853aa5c61b9a51d3cd655a06
allen <allenmuhani@gmail.com>
2026-01-16 14:55:55 +0300

    added explanation of the VRFD20RollDice script]

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -6,6 +6,13 @@ import { Script, console } from "forge-std/Script.sol";
 import { DevOpsTools } from "foundry-devops/src/DevOpsTools.sol";
 import { VRFD20 } from "../src/VRFD20.sol";
 
+/**
+ * @title VRFD roll dice.
+ * @author Allen Muhani.
+ * @notice Picks the most recently VRFD20 deployed on the current chain and runs its rollDice function.
+ * @notice To see the events and/find out if the fulfillRandomWords function was called emmiting the
+ *  DiceLanded eveent; check the smart contrct on etherscan events.
+ */
 contract VRFD20RollDice is Script {
     function rollDice(address mostRecentlyDeploy) public {
         vm.startBroadcast();
