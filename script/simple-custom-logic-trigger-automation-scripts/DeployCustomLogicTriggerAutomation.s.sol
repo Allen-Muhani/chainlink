172b76bd39c2584c43cf9849b804e9124ff0f9cc
allen <allenmuhani@gmail.com>
2026-01-21 14:47:59 +0300

    added documentation

--- script/simple-custom-logic-trigger-automation-scripts/DeployCustomLogicTriggerAutomation.s.sol
+++ script/simple-custom-logic-trigger-automation-scripts/DeployCustomLogicTriggerAutomation.s.sol
@@ -7,6 +7,11 @@ import {
     CustomLogicCounterTrigger
 } from "../../src/CustomLogicCounterTrigger.sol";
 
+/**
+ * @title Deploys a simple custom logic trigger automation.
+ * @author Allen Muhani
+ * @notice forge script script/simple-custom-logic-trigger-automation-scripts/DeployCustomLogicTriggerAutomation.s.sol   --rpc-url $SEPOLIA_RPC_URL   --private-key $PRIVATE_KEY   --broadcast   --verify   --etherscan-api-key $ETHERSCAN_API_KEY
+ */
 contract DeployCustomLogicTriggerAutomation is Script {
     function run() external returns (CustomLogicCounterTrigger) {
         vm.startBroadcast();
--- script/simple-custom-logic-trigger-automation-scripts/Interactions.s.sol
+++ script/simple-custom-logic-trigger-automation-scripts/Interactions.s.sol
@@ -6,6 +6,11 @@ import { Script, console } from "forge-std/Script.sol";
 import { DevOpsTools } from "foundry-devops/src/DevOpsTools.sol";
 import { Counter } from "../../src/Counter.sol";
 
+/**
+ * @title Get the numer of times the automation calls have happened.
+ * @author Allen Muhani
+ * @notice forge script script/simple-custom-logic-trigger-automation-scripts/Interactions.s.sol:CounterGetCount   --rpc-url $SEPOLIA_RPC_URL   --private-key $PRIVATE_KEY   --broadcast
+ */
 contract CounterGetCount is Script {
     function getCount(address mostRecentlyDeploy) public {
         vm.startBroadcast();
