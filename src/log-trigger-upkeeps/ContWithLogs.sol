9cdb9e919b80d3901abe42f1e030d3dafdb79d3b
allen <allenmuhani@gmail.com>
2026-01-22 14:15:58 +0300

    documentated the IlogAutomation interface

--- src/log-trigger-upkeeps/ContWithLogs.sol
+++ src/log-trigger-upkeeps/ContWithLogs.sol
@@ -2,13 +2,38 @@
 
 pragma solidity ^0.8.20;
 
+/// @notice Represents a log emitted by a contract in a transaction.
+/// @dev This struct contains important metadata about the log, including
+///      the index, timestamp, transaction hash, and additional data.
 struct Log {
-    uint256 index; // Index of the log in the block.
-    uint256 timestamp; // Timestamp of the block containing the log.
-    bytes32 txHash; // Hash of the transaction containing the log.abi
-    uint256 blockNumber; // Number of the block containing the log.
-    address source; // address of the contract that emitted the log.abi
-    bytes32[] topics; // Indexed topics of the log.
+    /// @notice Index of the log within the block.
+    /// @dev This field identifies the order of the log in the block.
+    uint256 index;
+
+    // Timestamp of the block containing the log.
+    // @dev the time when the block was mined.
+    uint256 timestamp;
+
+    /// @notice Represents a log emitted by a contract in a transaction.
+    /// @dev This struct contains important metadata about the log, including
+    ///      the index, timestamp, transaction hash, and additional data.
+    bytes32 txHash;
+
+    /// @notice Block number where the log was created/ number of block containing the log..
+    /// @dev The block number represents the position of the block in the blockchain.
+    uint256 blockNumber;
+
+    /// @notice Address of the contract that emitted the log.
+    /// @dev This address points to the contract (or account) that triggered the log.
+    address source;
+
+    /// @notice Array of indexed topics associated with the log.
+    /// @dev Topics are used for efficient filtering and searching of logs.
+    ///      The first topic is usually the event signature, followed by indexed parameters.
+    bytes32[] topics;
+
+    /// @notice Data associated with the log.
+    /// @dev This field contains the non-indexed data, usually event arguments or other payload.
     bytes data; // Data of the log.
 }
 
