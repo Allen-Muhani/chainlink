3ae779ab8380e687d461335f7172e434e952ee9b
allen <allenmuhani@gmail.com>
2026-01-22 14:27:06 +0300

    documented CountEmitLog.s

--- src/log-trigger-upkeeps/CountEmitLog.sol
+++ src/log-trigger-upkeeps/CountEmitLog.sol
@@ -2,11 +2,19 @@
 
 pragma solidity ^0.8.0;
 
+/**
+ * @title Count Emit Log
+ * @author Allen Muhani
+ * @notice emmits logs to be used by chainlink for autoamted calls.
+ */
 contract CountEmitLog {
     event WantsToCount(address indexed msgSender);
 
     constructor() { }
 
+    /**
+     * Just emits the expected log.
+     */
     function emitCountLog() external {
         emit WantsToCount(msg.sender);
     }
